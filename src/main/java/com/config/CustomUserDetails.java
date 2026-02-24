c9caef9c02b4ceac2a1724eff84df7ae279f0405
sanjay pidishetty <spidishetty@csod.com>
2026-02-24 22:06:30 +0530

    updating springboot from 2.7.8 to 2.7.18

--- src/main/java/com/config/CustomUserDetails.java
+++ src/main/java/com/config/CustomUserDetails.java
@@ -3,7 +3,6 @@ package com.config;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
